b8fa0b12d35a25e9643fd7d5b7f6f1b9c187c29a
panivnyk <98265791+panivnyk@users.noreply.github.com>
2022-10-24 18:44:42 +0000

    Deploying to gh-pages from @ panivnyk/goit-react-hw-05-movies@ce7e7f405293b327f22e60737a4d1cd985cfe88e 🚀

--- static/js/134.66bd0454.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[134],{134:function(e,i,s){s.r(i);var c=s(184);i.default=function(){return(0,c.jsx)("main",{children:(0,c.jsx)("h2",{children:"Movies search page"})})}}}]);
-//# sourceMappingURL=134.66bd0454.chunk.js.map
